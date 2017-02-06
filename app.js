7364651d4d1d32c5c9c39ad565e9e22971522d33
lvalido <lvalido@mailexternos.com.ar>
2017-02-06 12:31:23 -0300

    [MervalDoesnotStain] Basic Getting

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var $ = require('cheerio');
 var request = require('sync-request');
 var util = require('util');
 var fs = require('fs');
+var fileName = './dolar.json';
 
 function pad(n, width, z) {
   z = z || '0';
@@ -11,8 +12,13 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-app.get('/value', function (req, res) {
-  res.send('Dolar prediction for future!');
+app.get('/getrate/:month', function (req, res) {
+  var rawData = fs.readFileSync(fileName, 'utf8');
+  var fee = JSON.parse(rawData);
+  var el = fee.values.find ( o => o.month == parseInt(req.params.month) );
+  if (el){
+    res.send(el.value);
+  }
 })
 
 app.get('/process', function (req, res) {
@@ -23,6 +29,9 @@ app.get('/process', function (req, res) {
     var key='DLR%s2017';
     var url = 'https://www.rofex.com.ar';
 
+    var rawData = fs.readFileSync(fileName, 'utf8');
+    var fee = JSON.parse(rawData);
+
     var html = request('GET',url);
     var rawHtml = html.getBody('utf8');
     var node = $.load(rawHtml);
@@ -32,11 +41,16 @@ app.get('/process', function (req, res) {
 
       var value = node(selector).first().next().text();
       if (value != '') {
-        fee.values.push({month: i, value: value});
+        var el = fee.values.find ( o => o.month == i);
+        if (el){
+          el.value= value;
+        }else{
+          fee.values.push({month: i, value: value});
+        }
       }
     }
     var rawOutput = JSON.stringify( fee );
-    fs.writeFile('./dolar.json', rawOutput);
+    fs.writeFile(fileName, rawOutput);
     res.send(rawOutput + ' <BR/> Process OK');
 })
 
