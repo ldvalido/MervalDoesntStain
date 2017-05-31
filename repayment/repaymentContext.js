69bff5e9e1b932faba80ce91592f318146cdda34
lvalido <lvalido@mailexternos.com.ar>
2017-05-31 01:00:59 -0300

    [MervalDoesnotStain] Strategy for Repayment

---
+++ repayment/repaymentContext.js
@@ -0,0 +1,13 @@
+var Q = require('q');
+
+function calculateRepayment(returnValue, title,year,month, amount) {
+ 	var q = Q.defer();
+ 	var operations = {
+ 		
+ 	}
+	return q.promise;
+}
+
+module.exports = {
+	calculateRepayment
+}
